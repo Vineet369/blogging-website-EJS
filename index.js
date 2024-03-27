611315903f476741c71007d93059000e50887aa8
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2024-03-27 12:10:09 +0530

    blog upload page added, rendered cards of blogs on home page

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-const userRoute = require('./routes/user.route.js');
+const Blog = require('./models/blogs.models.js')
+
 const { checkForAuthenticationCookie } = require('./midddleware/auth.middleware.js');
+
+const userRoute = require('./routes/user.route.js');
+const blogRoute = require('./routes/blog.route.js');
+
 
 const app = express();
 const PORT = 8000;
@@ -19,17 +24,20 @@ app.set('views', path.resolve("./views"));
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"))
+app.use(express.static(path.resolve('./public'))) //let expres know to serve it as static
 
-app.get("/", (req, res) => {
-    console.log("req.user",req.user)
+app.get("/",async (req, res) => {
+    const allBlogs = await Blog.find({});
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
-}) 
+})  
